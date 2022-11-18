de3c3b272d9506729d464299ee602072d30a046b
Jason Cruz <jac024@ucsd.edu>
2022-11-18 10:48:18 -0800

    age func, testing

--- pkg/funcs/functions_test.go
+++ pkg/funcs/functions_test.go
@@ -7,39 +7,41 @@ import (
 	"github.com/cloudprivacylabs/opencypher"
 )
 
+type ageTest struct {
+	query    string
+	expected int
+}
+
 func TestAge(t *testing.T) {
-	// return age(parseDate("01/01/2022","MM/DD/YYYY"))
-	v1, err := opencypher.ParseAndEvaluate(`return age(parseDate("01/01/2000", "MM/DD/YYYY"))`, opencypher.NewEvalContext(lpg.NewGraph()))
-	if err != nil {
-		t.Error(err)
+	rGetter := func(v opencypher.Value) interface{} {
+		return v.Get().(opencypher.ResultSet).Rows[0]["1"].Get()
 	}
-	age1 := v1.Get().(opencypher.ResultSet).Rows[0]["1"].Get()
-	if age1 != 22 {
-		t.Errorf("Got age: %d, expected age: %d", age1, 22)
+	ageTests := []ageTest{
+		{
+			query:    `return age(parseDate("01/01/2000", "MM/DD/YYYY"))`,
+			expected: 22,
+		},
+		{
+			query:    `return age(parseDate("01/01/1900", "MM/DD/YYYY"), parseDate("01/01/2022", "MM/DD/YYYY"))`,
+			expected: 122,
+		},
+		{
+			query:    `return age(parseDate("01/01/1678", "MM/DD/YYYY"), parseDate("01/01/2022", "MM/DD/YYYY"))`,
+			expected: 344,
+		},
+		{
+			query:    `return age(parseDate("01/01/1677", "MM/DD/YYYY"), parseDate("01/01/2022", "MM/DD/YYYY"))`,
+			expected: 345,
+		},
 	}
-	// max dob - 1678
-	v2, err := opencypher.ParseAndEvaluate(`return age(parseDate("01/01/1900", "MM/DD/YYYY"), parseDate("01/01/2022", "MM/DD/YYYY"))`, opencypher.NewEvalContext(lpg.NewGraph()))
-	if err != nil {
-		t.Error(err)
-	}
-	age2 := v2.Get().(opencypher.ResultSet).Rows[0]["1"].Get()
-	if age2 != 122 {
-		t.Errorf("Got age: %d, expected age: %d", age2, 122)
-	}
-	v3, err := opencypher.ParseAndEvaluate(`return age(parseDate("01/01/1678", "MM/DD/YYYY"), parseDate("01/01/2022", "MM/DD/YYYY"))`, opencypher.NewEvalContext(lpg.NewGraph()))
-	if err != nil {
-		t.Error(err)
-	}
-	age3 := v3.Get().(opencypher.ResultSet).Rows[0]["1"].Get()
-	if age3 != 344 {
-		t.Errorf("Got age: %d, expected age: %d", age3, 344)
-	}
-	v4, err := opencypher.ParseAndEvaluate(`return age(parseDate("01/01/1677", "MM/DD/YYYY"), parseDate("01/01/2022", "MM/DD/YYYY"))`, opencypher.NewEvalContext(lpg.NewGraph()))
-	if err != nil {
-		t.Error(err)
-	}
-	age4 := v4.Get().(opencypher.ResultSet).Rows[0]["1"].Get()
-	if age4 != 345 {
-		t.Errorf("Got age: %d, expected age: %d", age4, 345)
+	ctx := opencypher.NewEvalContext(lpg.NewGraph())
+	for _, tt := range ageTests {
+		v, err := opencypher.ParseAndEvaluate(tt.query, ctx)
+		if err != nil {
+			t.Error(err)
+		}
+		if rGetter(v) != tt.expected {
+			t.Errorf("Got age: %d, expected age: %d", rGetter(v), tt.expected)
+		}
 	}
 }
