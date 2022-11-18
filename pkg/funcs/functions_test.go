8f139d5231affcea606aeb73eacce1a04fec1c66
Jason Cruz <jac024@ucsd.edu>
2022-11-18 10:48:14 -0800

    age func, testing

--- pkg/funcs/functions_test.go
+++ pkg/funcs/functions_test.go
@@ -26,5 +26,20 @@ func TestAge(t *testing.T) {
 	if age2 != 122 {
 		t.Errorf("Got age: %d, expected age: %d", age2, 122)
 	}
-	t.Fail()
+	v3, err := opencypher.ParseAndEvaluate(`return age(parseDate("01/01/1678", "MM/DD/YYYY"), parseDate("01/01/2022", "MM/DD/YYYY"))`, opencypher.NewEvalContext(lpg.NewGraph()))
+	if err != nil {
+		t.Error(err)
+	}
+	age3 := v3.Get().(opencypher.ResultSet).Rows[0]["1"].Get()
+	if age3 != 344 {
+		t.Errorf("Got age: %d, expected age: %d", age3, 344)
+	}
+	v4, err := opencypher.ParseAndEvaluate(`return age(parseDate("01/01/1677", "MM/DD/YYYY"), parseDate("01/01/2022", "MM/DD/YYYY"))`, opencypher.NewEvalContext(lpg.NewGraph()))
+	if err != nil {
+		t.Error(err)
+	}
+	age4 := v4.Get().(opencypher.ResultSet).Rows[0]["1"].Get()
+	if age4 != 345 {
+		t.Errorf("Got age: %d, expected age: %d", age4, 345)
+	}
 }
